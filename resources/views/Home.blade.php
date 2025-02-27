35b3676a98737728e3e8fd961f83ce729d79a156
Aan Ardiyana <aan@crew.id>
2025-02-28 01:09:43 +0700

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,3 @@
+@section('content')
+    <h1>Hello Dunia</h1>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Selamat Datang di MPP Kab. Serang</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+
+    @include('layouts.partials.header');
+
+    @yield('content')
+
+    @include('layouts.partials.header');
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    @stack('scripts')
+
+</body>
+
+</html>
