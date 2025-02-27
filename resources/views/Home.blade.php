afd6203b2c13b1e4f171d4269fd87d3991c5369b
Aan Ardiyana <aan@crew.id>
2025-02-28 01:53:12 +0700

    create hero section, and value section

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,3 +1,64 @@
+@extends('layouts.master')
+
 @section('content')
-    <h1>Hello Dunia</h1>
+    <div class="container">
+        <section class="hero">
+            <div class="hero-bg" style="background: url('{{ asset('images/hero-01.png') }}') no-repeat center center/cover;">
+            </div>
+            <div class="hero-content">
+                <h1>Siap Melayani dan Memudahkan</h1>
+                <p>Kami hadir untuk memberikan layanan terbaik dengan proses yang lebih mudah dan cepat.</p>
+                <a href="#" class="btn btn-main">Pelajari Lebih Lanjut</a>
+            </div>
+        </section>
+
+        {{-- Section value --}}
+        <section>
+            <div class="section-title">
+                <h2 class="text-center mb-5">Nilai Yang Ditanamkan</h2>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+                        <div class="card">
+                            <div class="card-body text-centers">
+                                <i class="ph ph-atom fs-1"></i>
+                                <h5 class="card-title mt-3">Terintegrasi</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis inventore quam
+                                    voluptates excepturi veniam.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+                        <div class="card">
+                            <div class="card-body text-centers">
+                                <i class="ph ph-atom fs-1"></i>
+                                <h5 class="card-title mt-3">Terintegrasi</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis inventore quam
+                                    voluptates excepturi veniam.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+                        <div class="card">
+                            <div class="card-body text-centers">
+                                <i class="ph ph-atom fs-1"></i>
+                                <h5 class="card-title mt-3">Terintegrasi</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis inventore quam
+                                    voluptates excepturi veniam.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+                        <div class="card">
+                            <div class="card-body text-centers">
+                                <i class="ph ph-atom fs-1"></i>
+                                <h5 class="card-title mt-3">Terintegrasi</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis inventore quam
+                                    voluptates excepturi veniam.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,15 +14,18 @@
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
 
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <script src="https://unpkg.com/@phosphor-icons/web@2.1.1"></script>
+
 </head>
 
 <body>
 
-    @include('layouts.partials.header');
+    @include('layouts.partials.header')
 
     @yield('content')
 
-    @include('layouts.partials.header');
+    @include('layouts.partials.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
